a6f94154a9feeb51ffc44e2e1e8ff5798a50f93d
hamzehhatamleh <101259803+hamzehhatamleh@users.noreply.github.com>
2026-07-07 16:32:11 +0300

    Add files via upload

---
+++ Mail-Merge-Day24/main.py
@@ -0,0 +1,27 @@
+
+
+
+#
+# file =  open("hamzeh.txt")
+# content = file.read()
+#
+# print(content)
+
+
+#
+# # a append  w write ,
+# with open("hamzeh.txt", mode="r") as file:
+#     content = file.readlines()
+#     print(content)
+
+
+
+text = " \t\n Hello World!  d  \n"
+print(text)
+cleaned = text.strip()
+
+print(f"'{cleaned}'")
+# Output: 'Hello World!'
+
+
+
